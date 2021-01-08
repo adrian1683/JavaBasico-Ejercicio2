af3b00366d738c929df6e2b65adf60d68babf0a3
Adrian <adrian.quintero.monterrosa@gmail.com>
2021-01-08 10:33:52 -0500

    últimos cambios en el proyecto Beneficios por COVID19

--- src/com/clases/BeneficiosCovid19.java
+++ src/com/clases/BeneficiosCovid19.java
@@ -1,15 +1,43 @@
 package com.clases;
 
 public class BeneficiosCovid19 {
-    //Debe ser el numero entero aleatorio puedes usar la funcion ramdom para esto
+
     private String id;
     private String nombre;
     private Float valorSubsidio;
 
-    //Completar clase
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getValorSubsidio() {
+        return valorSubsidio;
+    }
+
+    public void setValorSubsidio(Float valorSubsidio) {
+        this.valorSubsidio = valorSubsidio;
+    }
 
     //Implementar un metodo que retorne el mejor beneficio comparando los valores de todos los beneficios
-    public Float getMejorbeneficios(){
-        return Float.valueOf(0);
+    public Float getMejorbeneficios(float beneficio1, float beneficio2){
+        if(beneficio1 > beneficio2){
+            return Float.valueOf(beneficio1);
+        }
+        else{
+            return Float.valueOf(beneficio2);
+        }
+
     }
 }
--- src/com/clientes/Main.java
+++ src/com/clientes/Main.java
@@ -1,44 +1,96 @@
 package com.clientes;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
+import com.clases.BeneficiosCovid19;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code her
 
-        /**
-         * Crear 2 lista de beneficios para clientes Lista 1 y Lista 2 por ejemplo
-         * Ejemplo:
-         * id:12121
-         * nombre: Beneficiados que perdieron su trabajo por COVID19
-         * valorSubsidio: 150000
-         *
-         * Ejemplo:
-         * id:895656
-         * Nombre: Beneficios para persona tercera edad dagnificados COVID
-         * valorSubsidio:200000
-         */
+        Main metodos = new Main();
+        String idBeneficio1 = metodos.getIdBeneficio();
+        String idBeneficio2 = metodos.getIdBeneficio();
 
+        BeneficiosCovid19 beneficio1 = new BeneficiosCovid19();
+        BeneficiosCovid19 beneficio2 = new BeneficiosCovid19();
 
+        beneficio1.setId(idBeneficio1);
+        beneficio1.setNombre("Subsidio económico para personas que perdieron su empleo por COVID19");
+        beneficio1.setValorSubsidio(250000f);
 
+        beneficio2.setId(idBeneficio2);
+        beneficio2.setNombre("Subsidio económico para personas de tercera edad");
+        beneficio2.setValorSubsidio(160000f);
+
+        List<BeneficiosCovid19> listaBeneficio1 = new ArrayList<>();
+        List<BeneficiosCovid19> listaBeneficio2 = new ArrayList<>();
+
+        listaBeneficio1.add(beneficio1);
+        listaBeneficio2.add(beneficio2);
+
+        metodos.reporteLista(listaBeneficio1,listaBeneficio2);
 
     }
 
-    /**
-    * Implementa un metodo que entregue un reporte de comparacion de las 2 listas este metodo solo imprimira regiostros
-    * Cuando un metodo no retorna un tipo de datos se trabaja con la palabra clave VOID
-    * Abarcar los operadores terniarios para condiciones logicas
-    * Operadores logicos
-    * Operacionres aritmeticos
-    **/
+    public void reporteLista(List<BeneficiosCovid19> a, List<BeneficiosCovid19> b){
+        System.out.println("REPORTE DE COMPARACIÓN DE BENEFICIOS POR COVID19");
+        System.out.println("------------------------------------------------");
+        BeneficiosCovid19 ben = new BeneficiosCovid19();
+        for(int i=0; i<a.size(); i++){
+            for(int j=0; j<b.size(); j++){
+                if(a.get(i).getId().equals(b.get(j).getId())){
+                    System.out.println("Los IDs de los beneficios son iguales");
+                    System.out.println(a.get(i).getId());
+                    System.out.println(" ");
+                }
+                else{
+                    System.out.println("Beneficios recibidos por la pandemia - COVID19 -:");
+                    System.out.println(a.get(i).getId() + " : " + a.get(i).getNombre());
+                    System.out.println(b.get(j).getId() + " : " + b.get(j).getNombre());
+                    System.out.println(" ");
+                }
+                if(a.get(i).getNombre().equals(b.get(j).getNombre())){
+                    System.out.println("El beneficio es el mismo, aunque tengan diferente ID");
+                    System.out.println(a.get(i).getId() + " : " + a.get(i).getNombre());
+                    System.out.println(b.get(j).getId() + " : " + b.get(j).getNombre());
+                    System.out.println(" ");
+                }
+                else{
+                    System.out.println("Los beneficios son:");
+                    System.out.println(a.get(i).getNombre());
+                    System.out.println(b.get(j).getNombre());
+                    System.out.println(" ");
+                }
+                if (a.get(i).getValorSubsidio().equals(b.get(j).getValorSubsidio())){
+                    System.out.println("El valor del subsidio es igual en ambos beneficios");
+                    System.out.println("$"+a.get(i).getValorSubsidio() + " para " + a.get(i).getNombre());
+                    System.out.println("$"+a.get(i).getValorSubsidio() + " para " + b.get(j).getNombre());
+                    System.out.println(" ");
+                }
+                else{
+                    System.out.println("Valor recibido por beneficio:");
+                    System.out.println("$"+a.get(i).getValorSubsidio() + " para " + a.get(i).getNombre());
+                    System.out.println("$"+b.get(j).getValorSubsidio() + " para " + b.get(j).getNombre());
+                    System.out.println(" ");
+                    System.out.println("El mejor beneficio tiene un valor de: ");
+                    Float mejorBen = ben.getMejorbeneficios(a.get(i).getValorSubsidio(),b.get(j).getValorSubsidio());
+                    System.out.println("$"+mejorBen);
+                }
+            }
+        }
+    }
 
-
-
-    //Implementa un metodo que te entregue un numero aletorio para el ide de los beneficios usar el wrapper de Integer
     private String getIdBeneficio(){
-        //La Clase Math tiene varios metodos que te ayudaran
-        return"";
+
+        int numero = (int)(10000 * Math.random());
+
+        String numeroFinal = "" + numero;
+
+        for(int i = numeroFinal.length(); i < 4; i++){
+            numeroFinal = numeroFinal.concat("0");
+        }
+        return numeroFinal;
     }
 }
