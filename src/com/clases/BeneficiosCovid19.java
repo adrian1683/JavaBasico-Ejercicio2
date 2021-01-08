a267250053fa9f56b9acae2fa9227fff4dc333e4
Adrian <adrian.quintero.monterrosa@gmail.com>
2021-01-08 10:46:16 -0500

    corrección método mejor beneficio

--- src/com/clases/BeneficiosCovid19.java
+++ src/com/clases/BeneficiosCovid19.java
@@ -30,14 +30,16 @@ public class BeneficiosCovid19 {
         this.valorSubsidio = valorSubsidio;
     }
 
-    //Implementar un metodo que retorne el mejor beneficio comparando los valores de todos los beneficios
+
     public Float getMejorbeneficios(float beneficio1, float beneficio2){
-        if(beneficio1 > beneficio2){
+        if(beneficio1 == beneficio2){
             return Float.valueOf(beneficio1);
         }
-        else{
+        else if(beneficio1 > beneficio2){
+            return Float.valueOf(beneficio1);
+
+        }else{
             return Float.valueOf(beneficio2);
         }
-
     }
 }
